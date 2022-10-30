1789de693c25ce8734d705c30951852b28d287be
Jurek Baumann <jurekbaumann@me.com>
2022-10-30 21:29:12 +0100

    Implement assignment 3

--- src/main/java/baumann/javaassignment/assignment/controller/MainViewController.java
+++ src/main/java/baumann/javaassignment/assignment/controller/MainViewController.java
@@ -1,10 +1,46 @@
+package baumann.javaassignment.assignment.controller;
+
+import baumann.javaassignment.assignment.data.Database;
+import baumann.javaassignment.assignment.model.Item;
+import baumann.javaassignment.assignment.model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.util.StringConverter;
 public class MainViewController implements Initializable, IController {
     private final User loggedInUser;
     private final Database database;
     private final Logger log = Logger.getLogger(this.getClass().getName());
     @FXML
     public Label welcomeText;
-    public void onLendItem() {
+    @FXML
+    public Label lendFeedback;
+    @FXML
+    public Label receiveFeedback;
+    @FXML
+    public Label labelModifyItem;
+    @FXML
+    public Label labelItemFeedback;
+    @FXML
+    public Label labelModifyItemFeedback;
+    @FXML
+    @FXML
+    public VBox itemPanel;
+    @FXML
+    public VBox modifyItemPanel;
+
+    @FXML
+    public TableView<Item> itemsTable;
+    public MainViewController(User loggedInUser, Database database) {
+        this.loggedInUser = loggedInUser;
+        this.database = database;
+    }
+
+    public void onLendItemClick() {
         int borrowerId;
         int itemId;
 
@@ -42,7 +78,7 @@ public class MainViewController implements Initializable, IController {
         items.set(items.indexOf(item), item);
     }
 
-    public void onReceiveItem() {
+    public void onReceiveItemClick() {
         receiveFeedback.setTextFill(Color.RED);
 
         int itemId;
@@ -75,9 +111,85 @@ public class MainViewController implements Initializable, IController {
         item.setBorrower(null);
         items.set(items.indexOf(item), item);
 
+    public void onAddItemClick() {
+        log.info("Add Item");
+        labelModifyItem.setText("Add Item");
+        selectedItem = null;
+        labelItemFeedback.setText("");
+        modifyItemTitle.setText("");
+        modifyItemAuthor.setText("");
+        toggleModifyItemPanel(true);
+    }
+
+    public void onEditItemClick() {
+        ObservableList<Item> selectedItems = itemsTable.getSelectionModel().getSelectedItems();
+        if (selectedItems.size() != 1) {
+            labelItemFeedback.setText("Please select exactly one item to edit.");
+            return;
+        }
+        labelItemFeedback.setText("");
+        selectedItem = selectedItems.get(0);
+        labelModifyItem.setText("Edit Item");
+        modifyItemTitle.setText(selectedItem.getTitle());
+        modifyItemAuthor.setText(selectedItem.getAuthor());
+        toggleModifyItemPanel(true);
+    }
+
+    public void onDeleteItemClick() {
+        ObservableList<Item> selectedItems = itemsTable.getSelectionModel().getSelectedItems();
+        if (selectedItems.isEmpty()) {
+            labelItemFeedback.setText("Please select at least one item.");
+            return;
+        }
+        labelItemFeedback.setText("");
+        items.removeAll(selectedItems);
+        items.forEach(item -> item.setId(items.indexOf(item) + 1));
+    }
+
+    private void toggleModifyItemPanel(boolean show) {
+        modifyItemPanel.setVisible(show);
+        itemPanel.setVisible(!show);
     }
 
 
+    public void onCancelModifyItem() {
+        log.info("cancel Item");
+        labelModifyItemFeedback.setText("");
+        toggleModifyItemPanel(false);
+    }
+
+    public void onSubmitModifyItem() {
+        if (modifyItemTitle.getText().isEmpty()) {
+            labelModifyItemFeedback.setText("Item title cannot be empty.");
+            return;
+        }
+        if (modifyItemAuthor.getText().isEmpty()) {
+            labelModifyItemFeedback.setText("Item author cannot be empty.");
+            return;
+        }
+        labelModifyItemFeedback.setText("");
+
+        if (selectedItem != null) {
+            selectedItem.setTitle(modifyItemTitle.getText());
+            selectedItem.setAuthor(modifyItemAuthor.getText());
+            items.set(items.indexOf(selectedItem), selectedItem);
+        } else {
+            Item newItem = new Item(items.size() + 1, modifyItemTitle.getText(), modifyItemAuthor.getText());
+            items.add(newItem);
+        }
+        toggleModifyItemPanel(false);
+    }
+
+
+    public void onItemSearchInput() {
+        String query = itemSearch.getText();
+        if (query.isEmpty()) {
+            itemsTable.setItems(items);
+        } else {
+            itemsTable.setItems(items.stream().filter(i -> i.getTitle().toLowerCase().contains(query.toLowerCase()) || i.getAuthor().toLowerCase().contains(
+                    query.toLowerCase())).collect(Collectors.toCollection(FXCollections::observableArrayList)));
+        }
+    }
     @Override
     public void shutdown() {
         database.setMembers(members);
--- src/main/java/baumann/javaassignment/assignment/data/Database.java
+++ src/main/java/baumann/javaassignment/assignment/data/Database.java
@@ -8,17 +8,30 @@ import java.util.List;
 
 public class Database {
     private final List<User> users;
+    private List<Item> items;
+    private final Logger log = Logger.getLogger(this.getClass().getName());
 
     public Database() {
+        items = new ArrayList<>();
         users = new ArrayList<>();
         users.addAll(List.of(new User("admin", "admin"), new User("user", "user"), new User("asd", "asd")));
 
     }
 
-    public List<User> getUsers() {
-        return users;
+        items.addAll(List.of(new Item(1, "The Lord of the Rings", "J.R.R. Tolkien"),
+                new Item(2, "War and Peace", "Leo Tolstoy"),
+                new Item(3, "Clean Code", "Robert C. Martin"),
+                new Item(4, "The Hitchhiker's Guide to the Galaxy", "Douglas Adams"),
+                new Item(5, "A Song of Ice and Fire", "George R.R. Martin")));
     }
 
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
     public User getUserByName(String name) {
         return users.stream().filter(u -> u.getUserName().equals(name)).findFirst().orElse(null);
     }
